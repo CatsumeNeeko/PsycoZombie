1a4e720bfe63612e2158f757f60b9c1f52f238a3
Joshua O'Grady <joshuaogrady19@gmail.com>
2024-01-23 14:46:30 +0000

    1

--- PsycoZombie/Assets/_Scripts/PlayerStats.cs
+++ PsycoZombie/Assets/_Scripts/PlayerStats.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerStats : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public bool hasToolbox = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+=======
+    public float maxHealth;
+>>>>>>> Stashed changes
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
