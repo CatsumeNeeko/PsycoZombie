ac464f3678cfc121e23bde3c5873e181698b25c6
beelzbub-art <agmal1909alex@gmail.com>
2024-01-23 14:22:53 +0000

    nailGet

--- PsycoZombie/Assets/_Scripts/PlayerStats.cs
+++ PsycoZombie/Assets/_Scripts/PlayerStats.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public bool hasToolbox = false;
+    public int nailAmmount = 0;
+    public int maxNailAmmount = 12;
     // Start is called before the first frame update
     void Start()
     {
